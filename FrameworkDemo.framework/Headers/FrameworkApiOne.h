7f17394c0add868fc2893e5a0ca776f484be3333
limaofuyuanzhang <397524331@qq.com>
2016-03-06 20:52:56 +0800

    commit framework

---
+++ FrameworkDemo.framework/Headers/FrameworkApiOne.h
@@ -0,0 +1,13 @@
+//
+//  FrameworkApiOne.h
+//  FrameworkDemo
+//
+//  Created by 林涛 on 16/1/18.
+//  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrameworkApiOne : NSObject
+- (void)sayHello;
+@end
---
+++ FrameworkDemo.framework/Headers/FrameworkDemo.h
@@ -0,0 +1,20 @@
+//
+//  FrameworkDemo.h
+//  FrameworkDemo
+//
+//  Created by 林涛 on 16/1/18.
+//  Copyright © 2016年 limaofuyuanzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FrameworkDemo/FrameworkApiOne.h>
+
+//! Project version number for FrameworkDemo.
+FOUNDATION_EXPORT double FrameworkDemoVersionNumber;
+
+//! Project version string for FrameworkDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkDemo/PublicHeader.h>
+
+
